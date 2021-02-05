ba72310a9acffee6baae5677a917b0d7e92a5827
hosein <hosienlooki0@gmail.com>
2021-02-06 00:08:51 +0330

    getparent id test inserted

---
+++ getParentIDtest.c
@@ -0,0 +1,34 @@
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+int main(){
+    
+    
+
+
+    for (int i=0;i<10;++i){
+        
+        int pid=fork();
+        if (pid==0){
+            
+            printf(1,"my pid  is %d",getpid());
+            printf(1,"parent pid  is %d",getParentID());
+            break;
+        }
+        else {
+            printf(1,"my pid  is %d",getpid());
+            printf(1,"child pid  is %d",pid);
+            wait();
+        }
+        printf(1,"****************************\n\n");
+    }
+
+
+return 0;
+
+}
+
+
