d1e45602a8405e1ac304cc9f95a0abb16ad7a0c7
hosein <hosienlooki0@gmail.com>
2021-02-06 10:42:20 +0330

    better parent id test file

--- getParentIDtest.c
+++ getParentIDtest.c
@@ -2,25 +2,27 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
+#include "spinlock.h"
 
 int main(){
     
     
-
+    
 
     for (int i=0;i<10;++i){
         
         int pid=fork();
         if (pid==0){
-            
-            printf(1,"my pid  is %d",getpid());
-            printf(1,"parent pid  is %d",getParentID());
+            sleep(50);
+            printf(1,"  ****IN CHILD***\n");
+            printf(1,"my pid  is %d\n",getpid());
+            printf(1,"parent pid  is %d\n",getParentID());
             break;
         }
         else {
-            printf(1,"my pid  is %d",getpid());
-            printf(1,"child pid  is %d",pid);
+            printf(1,"  ****IN PARENT***\n");
+            printf(1,"my pid  is %d\n",getpid());
+            printf(1,"child pid  is %d\n",pid);
             wait();
         }
         printf(1,"****************************\n\n");
